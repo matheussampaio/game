a8ca58af1a3da60f282d8b8e61a2b8bb7f8a113d
Matheus Sampaio <matheus@sampaio.us>
2020-04-10 15:48:38 -0500

    feat: add @types/pino and use a child logger

--- server/rooms/battle/room.ts
+++ server/rooms/battle/room.ts
@@ -1,37 +1,42 @@
 import { Room, Client } from 'colyseus'
-import { Schema, type, MapSchema } from '@colyseus/schema'
+import { Logger } from 'pino'
 
 import logger from '../../logger'
 import State from './state'
 
 export default class BattleRoom extends Room<State> {
   maxClients = 2
+  log: Logger
 
   onCreate(options: any) {
-    logger.info('BattleRoom created!', options)
+    this.log = logger.child({ roomId: this.roomId })
+    this.log.info('BattleRoom created!', options)
 
     this.setState(new State())
   }
 
   onJoin(client: Client) {
-    logger.info('Client joined', client.sessionId)
+    this.log.info('Client joined', client.sessionId)
 
     this.state.createPlayer(client.sessionId)
+
+    if (this.clients.length === this.maxClients) {
+      this.log.info('locking room', this.roomId)
+      this.lock()
+    }
   }
 
   onLeave(client: Client) {
-    this.broadcast(`${client.sessionId} left.`)
-
     this.state.removePlayer(client.sessionId)
   }
 
   onMessage(client: Client, data: any) {
-    logger.info(`BattleRoom received message from ${client.sessionId}: ${data}`)
+    this.log.info(`BattleRoom received message from ${client.sessionId}: ${data}`)
 
     this.state.movePlayer(client.sessionId, data)
   }
 
   onDispose() {
-    logger.info('Dispose BattleRoom')
+    this.log.info('Dispose BattleRoom')
   }
 }
