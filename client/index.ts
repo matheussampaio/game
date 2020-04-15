ad87275f62846c9f6b23f562b26b9cdf4c7143ee
Matheus Sampaio <matheus@sampaio.us>
2020-04-14 23:14:22 -0500

    feat: move and update box from the server side

--- client/index.ts
+++ client/index.ts
@@ -1,25 +1,48 @@
 import * as Colyseus from 'colyseus.js'
+import Phaser from 'phaser'
 
 import { getWebsocketURL } from './utils'
+
+let shape = null
+let graphics = null
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1000,
+  height: 1000,
+  scene: {
+    create() {
+      graphics = this.add.graphics({
+        lineStyle: {
+          width: 3,
+          color: 0xff00ff
+        },
+        fillStyle: {
+          color: 0x00ff00
+        }
+      });
+
+      shape = new Phaser.Geom.Rectangle(100, 100, 20, 20)
+    }
+  }
+}
+
+new Phaser.Game(config)
 
 const client = new Colyseus.Client(getWebsocketURL())
 
 client.joinOrCreate('battle')
-  .then((room) => {
-    console.log(room.sessionId, 'joined', room.name, room.id)
+  .then((room: Colyseus.Room) => {
+    room.state.onChange = (changes: Colyseus.DataChange[]) => {
+      changes.forEach((change: Colyseus.DataChange) => {
+        if (change.field === 'position') {
+          graphics.clear()
 
-    room.onStateChange.once((state) => {
-      console.log('initial state', state)
-    })
+          shape.x = change.value.x
+          shape.y = change.value.y
 
-    room.onStateChange((state) => {
-      console.log('state changed', state)
-    })
-
-    room.onMessage((message) => {
-      console.log('received message from server', message)
-    })
-  })
-  .catch((error) => {
-    console.error('JOIN ERROR:', error)
+          graphics.fillRectShape(shape);
+        }
+      })
+    }
   })
--- server/rooms/battle/room.ts
+++ server/rooms/battle/room.ts
@@ -1,42 +1,59 @@
-import { Room, Client } from 'colyseus'
-import { Logger } from 'pino'
+import { Room } from 'colyseus'
+import { Schema, type } from '@colyseus/schema'
+import { Body, Engine, World, Bodies } from 'matter-js'
 
-import logger from '../../logger'
-import State from './state'
+class Position extends Schema {
+  @type('number')
+  x = 0
+
+  @type('number')
+  y = 0
+}
+
+class State extends Schema {
+  @type(Position)
+  position = new Position()
+}
 
 export default class BattleRoom extends Room<State> {
   maxClients = 2
-  log: Logger
+  patchRate = 1000 / 60
 
-  onCreate(options: any) {
-    this.log = logger.child({ roomId: this.roomId })
-    this.log.info('BattleRoom created!', options)
+  engine: Engine
+  boxA: Body
 
+  onCreate() {
     this.setState(new State())
+
+    this.engine = Engine.create()
+
+    this.engine.world.gravity.y = 0.02
+
+    this.boxA = Bodies.rectangle(100, 100, 20, 20, {
+      friction: 0.005,
+      frictionAir: 0,
+      restitution: 0.9
+    })
+
+    World.add(this.engine.world, [
+      this.boxA,
+      // walls
+      Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+      Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+      Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
+      Bodies.rectangle(400, 606, 800, 50.5, { isStatic: true })
+    ])
+
+    this.setSimulationInterval((deltaTime) => this.update(deltaTime))
   }
 
-  onJoin(client: Client) {
-    this.log.info('Client joined', client.sessionId)
-
-    this.state.createPlayer(client.sessionId)
-
-    if (this.clients.length === this.maxClients) {
-      this.log.info('locking room', this.roomId)
-      this.lock()
-    }
+  onMessage() {
   }
 
-  onLeave(client: Client) {
-    this.state.removePlayer(client.sessionId)
-  }
+  update(deltaTime: number) {
+    Engine.update(this.engine, deltaTime)
 
-  onMessage(client: Client, data: any) {
-    this.log.info(`BattleRoom received message from ${client.sessionId}: ${data}`)
-
-    this.state.movePlayer(client.sessionId, data)
-  }
-
-  onDispose() {
-    this.log.info('Dispose BattleRoom')
+    this.state.position.x = this.boxA.position.x
+    this.state.position.y = this.boxA.position.y
   }
 }
